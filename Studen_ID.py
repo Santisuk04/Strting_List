1534e046e039960a2cda7c4a39335cfac2130c1e
Santisuk04 <137144071+Santisuk04@users.noreply.github.com>
2023-06-20 14:36:33 +0700

    Add files via upload

---
+++ Studen_ID.py
@@ -0,0 +1,8 @@
+stu_id = input()
+print("Student ID", stu_id)
+fac_code = stu_id [-2:]
+year_in = "25" + stu_id[:2]
+deg_code = stu_id[2]
+print("Faculty Code", fac_code)
+print("Enrollment Year",year_in)
+print("Academic Degree",deg_code)
